e8145793ad0fb73454a1e9da036d56024d851eca
Leonardo Cecchi <leonardo.cecchi@gmail.com>
2024-02-08 17:38:12 +0100

    feat: initial implementation of the WAL service (#5)
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

---
+++ internal/fileutils/doc.go
@@ -0,0 +1,2 @@
+// Package fileutils contains a set of useful functions to manage files
+package fileutils
---
+++ internal/wal/impl.go
@@ -0,0 +1,51 @@
+package wal
+
+import (
+	"context"
+
+	"github.com/cloudnative-pg/cnpg-i/pkg/wal"
+)
+
+// Implementation is the implementation of the identity service
+type Implementation struct {
+	wal.WALServer
+}
+
+// GetCapabilities gets the capabilities of the WAL service
+func (Implementation) GetCapabilities(
+	context.Context,
+	*wal.WALCapabilitiesRequest,
+) (*wal.WALCapabilitiesResult, error) {
+	return &wal.WALCapabilitiesResult{
+		Capabilities: []*wal.WALCapability{
+			{
+				Type: &wal.WALCapability_Rpc{
+					Rpc: &wal.WALCapability_RPC{
+						Type: wal.WALCapability_RPC_TYPE_ARCHIVE_WAL,
+					},
+				},
+			},
+			{
+				Type: &wal.WALCapability_Rpc{
+					Rpc: &wal.WALCapability_RPC{
+						Type: wal.WALCapability_RPC_TYPE_RESTORE_WAL,
+					},
+				},
+			},
+			{
+				Type: &wal.WALCapability_Rpc{
+					Rpc: &wal.WALCapability_RPC{
+						Type: wal.WALCapability_RPC_TYPE_STATUS,
+					},
+				},
+			},
+			{
+				Type: &wal.WALCapability_Rpc{
+					Rpc: &wal.WALCapability_RPC{
+						Type: wal.WALCapability_RPC_TYPE_SET_FIRST_REQUIRED,
+					},
+				},
+			},
+		},
+	}, nil
+}
